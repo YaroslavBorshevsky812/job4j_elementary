9a0c59e1f3b8ef37370b0c8920ebc3a83854cf88
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-05 18:12:39 +0300

    2. Тесты для идеальный вес [#335442]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,13 +2,14 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        double rsl = (height-100)*1.15;
+        double rsl = (height - 100) * 1.15;
         return rsl;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height-110)*1.15;;
-        return rsl;
+        double rsl = (height - 110) * 1.15;
+        double rsl1 = rsl;
+        return rsl1;
     }
 
     public static void main(String[] args) {
