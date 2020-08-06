21cedf27e3354f59dc2b21da662c0323b9490184
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-06 22:23:54 +0300

    9.2. Checkstyle [#335417]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,7 +1,9 @@
 package ru.job4j;
 
+import static java.lang.System.*;
+
 public class Main {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
