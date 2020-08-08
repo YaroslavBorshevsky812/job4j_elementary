c79f60ed4118f844a05801690ef3b5c7c104739f
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-08 10:58:35 +0300

    4.1.2. Глупый бот [#335348]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -10,8 +10,6 @@ public class DummyBot {
         } else if ("Пока.".equals(question)) {
 
             rsl = "До скорой встречи.";
-        } else {
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rsl;
     }
