f58886f94b73faffe7a5e4ea622030cc6a63eaca
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-11 19:00:40 +0300

    6.2.3. Перевернуть массив. [#335387]

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -6,24 +6,31 @@ public class Turn {
         array[0] = array[array.length - 1];
         array[array.length - 1] = buf;
 
-        for (int i = 0; i > array.length; i-- ) {
-            int some = array[0];
-            array[0] = array[array.length - 1];
-            array[array.length - 1] = some;
-        }
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
         }
-        return array;
+            System.out.println(" ");
+            for (int i = 1; i < array.length - i; i++) {
+                int some = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = some;
+            }
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i]);
+            }
+            return array;
+        }
+
+        public static void main (String[] args){
+            int mass[] = {1, 2, 3, 4, 5};
+            int mass1[] = {1, 2, 3, 4};
+
+
+            System.out.println("");
+            back(mass);
+            System.out.println("");
+            back(mass1);
+        }
     }
 
-    public static void main(String[] args) {
-        int mass[] = {1, 2, 3, 4, 5};
-        int mass1[] = {1, 2, 3, 4};
-
-        back(mass);
-        System.out.println();
-        back(mass1);
-    }
-}
 
