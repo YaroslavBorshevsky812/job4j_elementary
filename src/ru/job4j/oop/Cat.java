e8bb17703620641f51bd8b3bd177e01c524e33bd
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-17 16:55:40 +0300

    1.5. Поля объекта [#335545]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,7 +1,27 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+        String name;
+        String food;
+
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+    public void show() {
+        System.out.println("There are " + name + "'s food " + food);
+
+    }
+
+
     public static void main(String[] args) {
         Cat peppy  = new Cat();
+            peppy.food = "Котлета";
+            peppy.name = "black";
+            peppy.show();
     }
 }
